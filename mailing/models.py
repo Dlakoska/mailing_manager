33d60a9e63751a72e1555a471243b0161b860fdb
Titov Evgenii <dlakoska@mail.ru>
2024-11-05 01:39:30 +0300

    создал модели, мигрировал, для каждой модели реализовал CRUD с шаблонами

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name='Email адрес', unique=True)
+    full_name = models.CharField(max_length=150, verbose_name='Ф.И.О. клиента')
+    сomment = models.TextField(verbose_name='Комментарий', **NULLABLE)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, verbose_name='Тема письма')
+    message = models.TextField(verbose_name='Сообщение')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'Письмо'
+        verbose_name_plural = 'Письма'
+
+
+class Mailing(models.Model):
+    STATUS_CHOICES = [
+        ("created", "Создана"),
+        ("started", "Активна"),
+        ("completed", "Завершена"),
+    ]
+
+    PERIODICITY_CHOICES = [
+        ("daily", "Ежедневно"),
+        ("weekly", "Еженедельно"),
+        ("monthly", "Ежемесячно"),
+    ]
+
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    start_time = models.DateTimeField(verbose_name='Дата начала рассылки')
+    periodicity = models.CharField(max_length=10, verbose_name='Периодичность', choices=PERIODICITY_CHOICES)
+    status = models.CharField(max_length=10, verbose_name='Статус', choices=STATUS_CHOICES, default='created')
+    message = models.ForeignKey(Message, verbose_name='Сообщение', on_delete=models.CASCADE)
+    client = models.ManyToManyField(Client, verbose_name='Клиенты')
+    actual_end_time = models.DateTimeField(verbose_name='Дата завершения рассылки', **NULLABLE)
+
+
+class MailingAttempt(models.Model):
+    STATUS_CHOICES = [('success', 'Успешно'), ('failed', 'Не успешно')]
+
+    mailing = models.ForeignKey(Mailing, verbose_name='Рассылка', on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    server_response = models.TextField(verbose_name='Ответ почтового сервиса', **NULLABLE)
+    send_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Попытка {self.id} для рассылки {self.mailing.id}'
+
+    class Meta:
+        verbose_name = 'Попытка'
+        verbose_name_plural = 'Попытки'
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,112 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from mailing.forms import MailingForm, ClientForm, MessageForm
+from mailing.models import Mailing, Client, Message
+
+
+class MailingListView(ListView):
+    model = Mailing
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+
+
+class MailingUpdateView(UpdateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+    def get_success_url(self):
+        return reverse('mailing:mailing_detail', args=[self.kwargs.get('pk')])
+
+
+class MailingDeleteView(DeleteView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Mailing
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class MailingCreateView(CreateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+    def get_success_url(self):
+        return reverse('mailing:client_list')
+
+
+class ClientDeleteView(DeleteView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Client
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class ClientCreateView(CreateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageDeleteView(DeleteView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Message
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageCreateView(CreateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageUpdateView(UpdateView):
+    login_url = "users:login"
+    redirect_field_name = "redirect_to"
+
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list')
+
+    def get_success_url(self):
+        return reverse('mailing:message_list')
